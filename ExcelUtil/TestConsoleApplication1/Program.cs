58ed47507336ad6fad49a3d282587974746215f5
miracleQin <405415435@qq.com>
2014-12-27 11:09:47 +0800

    update1

---
+++ ExcelUtil/TestConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToList();
+        }
+        static void ToExcel() 
+        {
+            List<TestModel> list = new List<TestModel>();
+            list.Add(new TestModel() { DTValue=DateTime.Now, StrValue="test" });
+            list.Add(new TestModel() { IntValue=2});
+
+            Qin.ExcelUtil.ExcelUtil.ListToExcel<TestModel>(list, System.AppDomain.CurrentDomain.BaseDirectory+@"\test.xls");
+
+        }
+        static void ToList() 
+        {
+            var list = Qin.ExcelUtil.ExcelUtil.ExcelToList<TestModel>(@"C:\Users\user\Desktop\test.xls");
+            foreach (var item in list) 
+            {
+                Console.WriteLine(string.Format("int:{0} dateTime:{1} string:{2}", item.IntValue, item.DTValue, item.StrValue));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExcelUtil/TestConsoleApplication1/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication1
+{
+    [DisplayName("测试实体")]
+    public class TestModel
+    {
+        [DisplayName("整形字段")]
+        public int? IntValue { get; set; }
+        [DisplayName("字符串类型字段")]
+        public string StrValue { get; set; }
+        [DisplayName("时间类型字段")]
+        public DateTime? DTValue { get; set; }
+
+    }
+}
